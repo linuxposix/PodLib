90b6f920f3560efcac0efc0895a4501487873e1d
hackerTG <admin@hackerTGui-MacBook-Air.local>
2015-11-09 14:27:30 +0900

    라이브러리 초기 파일 추가

---
+++ PodLib/PodLib.h
@@ -0,0 +1,15 @@
+//
+//  PodLib.h
+//  PodLib
+//
+//  Created by hackerTG on 2015. 11. 9..
+//  Copyright © 2015년 HackersTechnicalGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodLib : NSObject
+
+- (void)greet;
+
+@end
---
+++ PodLib/PodLib.m
@@ -0,0 +1,18 @@
+//
+//  PodLib.m
+//  PodLib
+//
+//  Created by hackerTG on 2015. 11. 9..
+//  Copyright © 2015년 HackersTechnicalGroup. All rights reserved.
+//
+
+#import "PodLib.h"
+
+@implementation PodLib
+
+- (void)greet
+{
+    NSLog(@"헬로, 월드!");
+}
+
+@end
